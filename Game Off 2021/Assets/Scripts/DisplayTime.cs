c75dc1b2e1ded23deb45115f9054eb670ab80c42
BlueBonfire <bluebonfiregames@gmail.com>
2021-11-20 20:37:17 +0400

    commit 11
    little things

---
+++ Game Off 2021/Assets/Scripts/DisplayTime.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayTime : MonoBehaviour
+{
+    public GameObject theDisplay;
+    public int hour;
+    public int minutes;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hour = System.DateTime.Now.Hour;
+        minutes = System.DateTime.Now.Minute;
+        theDisplay.GetComponent<Text>().text = "" + hour + ":" + minutes;
+    }
+}
