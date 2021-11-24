34819bd173dd5b8f87c46c325549b95cff7d3c91
BlueBonfire <bluebonfiregames@gmail.com>
2021-11-24 21:53:17 +0400

    Commit16
    Final

---
+++ Game Off 2021/Assets/Scripts/WaitFinal.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class WaitFinal : MonoBehaviour
+{
+    public float waitTime = 16.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitForIntro());
+    }
+
+    IEnumerator WaitForIntro()
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        SceneManager.LoadScene(0);
+    }
+}
