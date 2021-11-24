2b5ec2e7e3668bdbf57dff1714e37ae85a5b15de
BlueBonfire <bluebonfiregames@gmail.com>
2021-11-24 15:21:47 +0400

    commit 14
    3 problem remain

--- Game Off 2021/Assets/Scripts/DisplayTime.cs
+++ Game Off 2021/Assets/Scripts/DisplayTime.cs
@@ -2,26 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using TMPro;
 
 public class DisplayTime : MonoBehaviour
 {
-    public GameObject theDisplay;
-    public int hour;
-    public int minutes;
-    string m;
-    // Start is called before the first frame update
-    void Start()
+    public TextMeshProUGUI theDisplay;
+    void Awake()
     {
-
 
     }
-
-    // Update is called once per frame
     void Update()
     {
-        hour = System.DateTime.Now.Hour;
-        minutes = System.DateTime.Now.Minute;
-        m = minutes.ToString();
-        theDisplay.GetComponent<Text>().text = "" + hour + ":" + m;
+        DateTime time = DateTime.Now;
+        string hour = LeadingZero(time.Hour);
+        string minute = LeadingZero(time.Minute);
+        theDisplay.text = hour + ":" + minute;
+    }
+    string LeadingZero(int n)
+    {
+        return n.ToString().PadLeft(2, '0');
     }
 }
--- Game Off 2021/Assets/Scripts/PauseMenu.cs
+++ Game Off 2021/Assets/Scripts/PauseMenu.cs
@@ -1,11 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject menuPanel;
+    public Slider volumeSlider;
+
+    public float startVolume = 1f;
+    public float currentVolume;
+
+    string newVolume = "VOLUME_SLIDER"; //Can change to whatever you want as long as it is not null or empty
+
+
+    void Start()
+    {
+        //Load volume from prevois saved state. If it does not exist, use 1 as default
+        volumeSlider.value = PlayerPrefs.GetFloat(newVolume, 1);
+        currentVolume = volumeSlider.value;
+    }
+
+    void UpdateVolume(float _volume)
+    {
+        currentVolume = _volume;
+
+        PlayerPrefs.SetFloat(newVolume, _volume);
+        PlayerPrefs.Save();
+    }
+    void OnEnable()
+    {
+        //Register Slider Events
+        volumeSlider.onValueChanged.AddListener(delegate { sliderCallBack(volumeSlider); });
+    }
+
+
+    private void sliderCallBack(Slider sliderMoved)
+    {
+        //Volume Slider Moved
+        if (sliderMoved == volumeSlider)
+        {
+            UpdateVolume(sliderMoved.value);
+        }
+    }
+    void OnDisable()
+    {
+        //Un-Register Slider Events
+        volumeSlider.onValueChanged.RemoveAllListeners();
+    }
+
+
+
+
+
     public void Menu()
     {
         SceneManager.LoadScene(2);
