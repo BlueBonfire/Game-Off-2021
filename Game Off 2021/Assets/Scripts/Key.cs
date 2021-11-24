1641569f418bf6c4a699b74aa9c5de49ce1c7346
BlueBonfire <bluebonfiregames@gmail.com>
2021-11-24 17:05:58 +0400

    commit15
    just final

--- Game Off 2021/Assets/Scripts/Key.cs
+++ Game Off 2021/Assets/Scripts/Key.cs
@@ -6,6 +6,7 @@ public class Key : MonoBehaviour
 {
     public GameObject block;
     public GameObject warningMsg;
+    public GameObject msg;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +26,7 @@ public class Key : MonoBehaviour
             Destroy(gameObject);
         }
         warningMsg.gameObject.SetActive(false);
+        msg.gameObject.SetActive(true);
 
         
     }
